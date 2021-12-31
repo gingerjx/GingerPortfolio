cea9e1066183c33ca57e00519fd66786a0e9031d
Piotr Kalota <piotrkalota99@gmail.com>
2021-12-31 14:02:05 +0100

    Add home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Links from './components/links/Links'
 import Nav from './components/nav/Nav';
+import Home from './components/home/Home'
 
 function App() {
   return (
     <div>
       <Links></Links>
       <Nav></Nav>
+      <Home></Home>
     </div>
   );
 }
