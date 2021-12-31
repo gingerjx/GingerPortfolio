3b8d59a07f568b0ae6c7742a2971939b4a0fd3b5
Piotr Kalota <piotrkalota99@gmail.com>
2021-12-31 15:58:27 +0100

    Add details button component

---
+++ src/components/common/details-button/DetailsButton.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import './detailsButton.css'
+import ArrowOrange from './assets/orange-arrow.svg'
+import ArrowBlack from './assets/black-arrow.svg'
+
+const DetailsButton = ({ isOrange }) => {
+    const arrow = isOrange ? ArrowOrange : ArrowBlack
+    const color = isOrange ? '#C26D30' : '#000000'
+    console.log(isOrange)
+    console.log(arrow)
+    console.log(color)
+
+    return (
+        <button 
+            className='clickable details-button'
+            style={ { color : color } }>
+            Details 
+            <img src={arrow} alt="Right arrow"></img>
+        </button>
+    )
+}
+
+export default DetailsButton
