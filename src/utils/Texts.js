c5cf88a609095e5aea028ad8a82b761fd7d3dce9
Piotr Kalota <piotrkalota99@gmail.com>
2022-01-02 16:53:44 +0100

    Add experience texts

--- src/utils/Texts.js
+++ src/utils/Texts.js
@@ -22,8 +22,47 @@ const Texts = {
                 content : "Computers are misterious creatures. But if you take time to get familiar with them, it turns out that they are not that scary. A few examples of <b>Operating Systems</b> solutions, can help you to get familiar with this topic.",
                 tags : "#OS #Memory #Management #C #Multithreading"
             }
+        },
+        experience : {
+            bachelor : {
+                position : "Student Computer Science Bachelor",
+                location : "Technology University of Lodz",
+                description : "TUL was an entity, which introduced me into world of Computer Science. Many modules, ranging from embedded systems to graphic design, helped me create a point of view what IT is.",
+                dates : "10.2018 - 02.2022"
+            },
+            samsung : {
+                position : "C/C++ Intern",
+                location : "Samsung R&D Poland",
+                description : "This internship is my first step into proffesional IT work. Togheter with team in Warsaw and Seul, we've been refactoring EFL's Evas library, what was a big lesson about C family and common tools used in Continous Integration.",
+                dates : "07.2020 - 07.2021"
+            },
+            iLab : {
+                position : "iLab Project Member",
+                location : "University of Applied Science in Sankt Polten",
+                description : "For one semester I had a pleasure to study in Austria. I participated in iLab project, which was based on Design Thinking Patterns of creating solution. My team - ToBinOrNotToBin - was looking for a solution in topic of food waste.",
+                dates : "02.2021 - 07.2021"
+            },
+            sollers : {
+                position : "Junior Java Developer",
+                location : "Sollers Consulting",
+                description : "During buying an insurance or claiming an accident, it's possible that you were using indirectly the systems written by me. As a Sollers member I was developing solutions for insurance clients in Guidewire framework.",
+                dates : "08.2021 - 02.2022"
+            },
+            pwc : {
+                position : "Junior Java Developer",
+                location : "PWC",
+                description : "To be written.",
+                dates : "02.2022 - Present"
+            },
+            masters : {
+                position : "Student Computer Science Master",
+                location : "Warsaw University of Technology",
+                description : "To be written.",
+                dates : "03.2022 - Present"
+            },
         }
     }
+
 }
 
 export default Texts
