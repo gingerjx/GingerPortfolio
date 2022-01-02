d7ed27e73df80a5e03bf30120a85f0fb7924e64c
Piotr Kalota <piotrkalota99@gmail.com>
2022-01-02 18:49:34 +0100

    Styled tiles of experience

--- src/components/home/experience/Experience.js
+++ src/components/home/experience/Experience.js
@@ -1,23 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './experience.css'
 import Tile from './tile/Tile'
 import Texts from '../../../utils/Texts'
 
 const Experience = () => {
+    const [bachelorActive, setBachelorActive] = useState(true)
+    const [samsungActive, setSamsungActive] = useState(false)
+    const [iLabActive, setILabActive] = useState(false)
+    const [sollersActive, setSollersActive] = useState(false)
+    const [pwcActive, setPwcActive] = useState(false)
+    const [masterActive, setMasterActive] = useState(false)
+
+    const bachelor = {
+        active : bachelorActive,
+        setActive : setBachelorActive,
+        clickEvent : () => onClick(bachelor),
+        ...Texts.home.experience.bachelor
+    }
+    const samsung = {
+        active : samsungActive,
+        setActive : setSamsungActive,
+        clickEvent : () => onClick(samsung),
+        ...Texts.home.experience.samsung
+    }
+    const iLab = {
+        active : iLabActive,
+        setActive : setILabActive,
+        clickEvent : () => onClick(iLab),
+        ...Texts.home.experience.iLab
+    }
+    const sollers = {
+        active : sollersActive,
+        setActive : setSollersActive,
+        clickEvent : () => onClick(sollers),
+        ...Texts.home.experience.sollers
+    }
+    const pwc = {
+        active : pwcActive,
+        setActive : setPwcActive,
+        clickEvent : () => onClick(pwc),
+        ...Texts.home.experience.pwc
+    }
+    const master = {
+        active : masterActive,
+        setActive : setMasterActive,
+        clickEvent : () => onClick(master),
+        ...Texts.home.experience.masters
+    }
+    const tiles = [bachelor, samsung, iLab, sollers, pwc, master]
+
     return (
         <div className='home-experience-container'>
-            <Tile data={Texts.home.experience.bachelor}></Tile>            
-            <Tile data={Texts.home.experience.samsung}></Tile>  
-            <Tile data={Texts.home.experience.iLab}></Tile>
+            <Tile data={bachelor}></Tile>            
+            <Tile data={samsung}></Tile>  
+            <Tile data={iLab}></Tile>
             <div style={{ 
                 backgroundColor : 'red', 
                 gridRow : '1 / span 2',
                 gridColumn : '4' }}>asfasf</div>  
-            <Tile data={Texts.home.experience.sollers}></Tile>  
-            <Tile data={Texts.home.experience.pwc}></Tile>  
-            <Tile data={Texts.home.experience.masters}></Tile>  
+            <Tile data={sollers}></Tile>  
+            <Tile data={pwc}></Tile>  
+            <Tile data={master}></Tile> 
         </div>
     )
+
+    function onClick(tile) {
+        for (const t of tiles) {
+            t.setActive(false)
+        }
+        tile.setActive(true)
+    }
 }
 
 export default Experience
--- src/components/home/experience/tile/Tile.js
+++ src/components/home/experience/tile/Tile.js
@@ -2,11 +2,17 @@ import React from 'react'
 import './tile.css'
 
 const Tile = ({ data }) => {
+    const styleActive = { background : 'linear-gradient(0deg, rgba(42,36,56,1) 10%, rgba(194,109,48,1) 100%)' }
+    const styleDisabled = { background : 'linear-gradient(0deg, rgba(42,36,56,1) 0%, rgba(42,36,56,0) 100%)' }
+    const background = data.active ? styleActive : styleDisabled;
+
     return (
-        <div className='home-experience-tile-container'>
+        <div onClick={data.clickEvent} style={background} className=' clickable home-experience-tile-container'>
             <p className='home-experience-tile-position'>{data.position}</p>
-            <p className='home-experience-tile-location'>{data.location}</p>         
-            <p className='home-experience-tile-date'>{data.dates}</p>
+            <p className='home-experience-tile-location'>{data.location}</p> 
+            <div className='home-experience-tile-date-wrapper'>
+                <p className='home-experience-tile-dates'>{data.dates}</p>
+            </div>        
         </div>
     )
 }
