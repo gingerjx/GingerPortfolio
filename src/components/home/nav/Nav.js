18b1012d28d01dc7662e4f7da4848e0c262fabff
Piotr Kalota <piotrkalota99@gmail.com>
2022-01-17 18:55:07 +0100

    Add auto activation of nav during scroll

--- src/components/home/nav/Nav.js
+++ src/components/home/nav/Nav.js
@@ -6,23 +6,33 @@ const Nav = ( { introId, highlightsId, experienceId, moreId }) => {
     const [intro, setIntro] = useState({
         title: 'Intro',
         active: true,
-        id: introId
+        id: introId,
+        yPos: 0
     })
     const [highlights, setHighlights] = useState({
         title: 'Highlights',
         active: false,
-        id: highlightsId
+        id: highlightsId,
+        yPos: 0
     })
     const [experience, setExperience] = useState({
         title: 'Experience',
         active: false,
-        id: experienceId
+        id: experienceId,
+        yPos: 0
     })
     const [more, setMore] = useState({
         title: 'More',
         active: false,
-        id: moreId
+        id: moreId,
+        yPos: 0
     })
+
+    useEffect(() => {
+        window.removeEventListener('scroll', onScroll);
+        window.addEventListener('scroll', onScroll, { passive: true });
+        return () => window.removeEventListener('scroll', onScroll);
+    }, []);
 
     const standardStyle = 'clickable nav-element';
 
@@ -37,13 +47,42 @@ const Nav = ( { introId, highlightsId, experienceId, moreId }) => {
     )
 
     function activeOnClick(element, setter) {
+        disableAll();
+        setter({...element, active : true});
+        
+        document.getElementById(element.id).scrollIntoView({behavior: "smooth"})
+    }
+    
+    function disableAll() {
         setIntro({...intro, active : false});
         setHighlights({...highlights, active : false});
         setExperience({...experience, active : false});
         setMore({...more, active : false});
-        setter({...element, active : true});
+    }
+
+    function onScroll() {
+        const introElement = document.getElementById(intro.id)
+        const highlightsElement = document.getElementById(highlights.id)
+        const experienceElement = document.getElementById(experience.id)
+        const moreElement = document.getElementById(more.id)
+
+        const introPositionY = Math.abs(introElement.getBoundingClientRect().top);
+        const highlightsPositionY = Math.abs(highlightsElement.getBoundingClientRect().top);
+        const experiencePositionY = Math.abs(experienceElement.getBoundingClientRect().top);
+        const morePositionY = Math.abs(moreElement.getBoundingClientRect().top);
+
+        const min = Math.min(introPositionY, highlightsPositionY, experiencePositionY, morePositionY)
         
-        document.getElementById(element.id).scrollIntoView({behavior: "smooth"})
+        disableAll();
+        if (introPositionY === min) {
+            setIntro({...intro, active : true})
+        } else if (highlightsPositionY === min) {
+            setHighlights({...highlights, active : true})
+        } else if (experiencePositionY === min) {
+            setExperience({...experience, active : true})
+        } else if (morePositionY === min) {
+            setMore({...more, active : true})
+        }
     }
 }
 
