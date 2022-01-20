ba9c23f4c5c7529dbacef20fa8ffcb379daabd52
Piotr Kalota <piotrkalota99@gmail.com>
2022-01-21 00:37:47 +0100

    Add missing microservice to cheaclo

--- src/components/cheaclo/Cheaclo.js
+++ src/components/cheaclo/Cheaclo.js
@@ -10,7 +10,13 @@ import seleniumIcon from './assets/selenium.png'
 import postgresIcon from './assets/postgresql.svg'
 import angularIcon from './assets/angular.png'
 import typescriptIcon from './assets/typescript.png'
+
 import crawlerIcon from './assets/crawler.svg'
+import clothesIcon from './assets/clothes-hanger.svg'
+import usersIcon from './assets/user.svg'
+import frontIcon from './assets/web-design.svg'
+import registryIcon from './assets/cloud.svg'
+import gatewayIcon from './assets/gateway.svg'
 
 const Cheaclo = () => {
   return <div className='cheaclo-container'>
@@ -18,7 +24,7 @@ const Cheaclo = () => {
             <div className='chapter'>
               <div className='content'>
                 <p>{Texts.cheaclo.aFewWordsText1}</p>
-                <p>{Texts.cheaclo.aFewWordsText2}</p>
+                <p dangerouslySetInnerHTML={{ __html: Texts.cheaclo.aFewWordsText2}}></p>
               </div>
               <ChapterTitle text={Texts.cheaclo.aFewWords}/>
             </div>
@@ -31,8 +37,36 @@ const Cheaclo = () => {
                   title={Texts.cheaclo.webScraper}
                   description={Texts.cheaclo.webScraperText}
                   tags={[seleniumIcon, springIcon, javaIcon]}/>
-                {/* <Microservice />
-                <Microservice /> */}
+                <Microservice 
+                  hrefTo={Texts.cheaclo.clothesDatabaseLink}
+                  icon={clothesIcon}
+                  title={Texts.cheaclo.clothesDatabase}
+                  description={Texts.cheaclo.clothesDatabaseText}
+                  tags={[postgresIcon, springIcon, javaIcon]}/>
+                <Microservice 
+                  hrefTo={Texts.cheaclo.usersDatabaseLink}
+                  icon={usersIcon}
+                  title={Texts.cheaclo.usersDatabase}
+                  description={Texts.cheaclo.usersDatabaseText}
+                  tags={[postgresIcon, springIcon, javaIcon]}/>
+                <Microservice 
+                  hrefTo={Texts.cheaclo.clientFrontLink}
+                  icon={frontIcon}
+                  title={Texts.cheaclo.clientFront}
+                  description={Texts.cheaclo.clientFrontText}
+                  tags={[angularIcon, typescriptIcon]}/>
+                <Microservice 
+                  hrefTo={Texts.cheaclo.serviceRegistryLink}
+                  icon={registryIcon}
+                  title={Texts.cheaclo.serviceRegistry}
+                  description={Texts.cheaclo.serviceRegistryText}
+                  tags={[springIcon, javaIcon]}/>
+                <Microservice 
+                  hrefTo={Texts.cheaclo.cloudGatewayLink}
+                  icon={gatewayIcon}
+                  title={Texts.cheaclo.cloudGateway}
+                  description={Texts.cheaclo.cloudGatewayText}
+                  tags={[springIcon, javaIcon]}/>
               </div>
               <ChapterTitle text={Texts.cheaclo.technology}/>
             </div>
