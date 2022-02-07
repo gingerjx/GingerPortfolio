1c285618377b6b657f64ae53041980b3041a9cb7
Piotr Kalota <piotrkalota99@gmail.com>
2022-02-07 19:11:35 +0100

    Finish machine learning highlights

--- src/components/ml/MachineLearning.js
+++ src/components/ml/MachineLearning.js
@@ -4,6 +4,7 @@ import Texts from '../../utils/Texts'
 import ChapterTitle from '../common/chapterTitle/ChapterTitle';
 import BackButton from '../common/backButton/BackButton'
 import Plots from './plot/Plots';
+import gif from './assets/animation.gif'
 
 const MachineLearning = () => {
   return <div className='cheaclo-container'>
@@ -34,8 +35,31 @@ const MachineLearning = () => {
               <div className='content'>
                 <p dangerouslySetInnerHTML={{ __html: Texts.ml.smartSnakeText1}}></p>
                 <p>{Texts.ml.smartSnakeText2}</p>
+                <ul>
+                  <li>{Texts.ml.smartSnakePoint1}</li>
+                  <li>{Texts.ml.smartSnakePoint2}</li>
+                  <li>{Texts.ml.smartSnakePoint3}</li>
+                  <li>{Texts.ml.smartSnakePoint4}</li>
+                  <li>{Texts.ml.smartSnakePoint5}</li>
+                  <li>{Texts.ml.smartSnakePoint6}</li>
+                  <li>{Texts.ml.smartSnakePoint7}</li>
+                </ul>
               </div>
               <ChapterTitle text={Texts.ml.smartSnake}/>
+            </div>
+            <div className='chapter'>
+              <div className='content'>
+                <p>{Texts.ml.presentationText}</p>
+                <img src={gif} alt='Snake gif'></img>
+                <p>
+                  {Texts.ml.smartSnakeDetails} 
+                  <a href={Texts.ml.smartSnakeLink} target="_blank" rel="noreferrer">
+                    <b>Github</b>
+                  </a>
+                  .
+                </p>
+              </div>
+              <ChapterTitle text={Texts.ml.presentation}/>
             </div>
           </div>
 };
