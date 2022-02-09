9aa24b824c091abb1d718e3a7b1ced5a44b01592
Piotr Kalota <piotrkalota99@gmail.com>
2022-02-09 20:52:32 +0100

    Add back button responsivness

--- src/components/common/backButton/BackButton.js
+++ src/components/common/backButton/BackButton.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import './backButton.css'
+import './backButton_big.css'
+import './backButton_small.css'
 import arrow from './assets/arrow.svg'
 import { Link } from 'react-router-dom'
 
